305e627f366f1f0727d596068be8070accc6a7bf
laramontana <larabrightside@gmail.com>
2017-11-29 14:29:12 +0300

    Done.

---
+++ src/components/foo.js
@@ -0,0 +1,7 @@
+export const message = "I am a component!";
+
+// In a file called `fruits.js`
+export default {
+  apple: 'red',
+  banana: 'yellow',
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// Code goes here
+import { message } from './components/foo';
+
+// In a file in the same directory
+import fruit from './fruits';
+console.log(fruit.apple); // prints 'red'
+
+// In another file, also in the same directory
+import { apple } from './fruits';
+console.log(apple); // prints 'red'
+
+// When using named exports, we can choose to either import the entire thing and then reference the keys on the exported object,
+//or we can import one specific key.
